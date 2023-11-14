a3bcb6040072206adcbbc9c6d0480979b4060feb
thanieLee <thancraft123gaming@gmail.com>
2023-11-14 20:02:22 +0800

    13.cpp and 14.cpp AC

--- AP CSCI 30/Week 10-11/bash.py
+++ AP CSCI 30/Week 10-11/bash.py
@@ -1,16 +1,3 @@
-arr1 = []
-arr2 = []
-
-f1 = open("output.txt", "r")
-
-for x in f1:
-    arr1.append(x)
-
-f2 = open("test_output (3).txt", "r")
-
-for x in f2:
-    arr2.append(x)
-
-for i in range(len(arr1)):
-    if arr1[i] != arr2[i]:
-        print(i+1, arr1[i], arr2[i])
+print(10**6)
+for i in range(1000000000, 1000000000-1000000, -1):
+    print(i, end=" ")
