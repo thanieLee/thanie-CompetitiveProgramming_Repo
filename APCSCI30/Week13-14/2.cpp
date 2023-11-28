18f747f65c3c84efb3cc386e20069c02b52170e0
thanieLee <thancraft123gaming@gmail.com>
2023-11-28 20:04:08 +0800

    APCSCI30 Week13-14

---
+++ APCSCI30/Week13-14/2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+vector<int> z(string s) {
+        int n = s.size();
+        vector<int> z(n);
+        int x = 0, y = 0;
+        for (int i = 1; i < n; i++) {
+           z[i] = max(0,min(z[i-x],y-i+1));
+           while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
+               x = i; y = i+z[i]; z[i]++;
+           }
+    }
+    return z;
+}
+ 
+int main(){
+    string s; cin >> s;
+
+    vector<int> ans = z(s + "#" + s);
+    for (int i = ans.size()-1; i >= ans.size()-s.size()+1; i--){
+        if (ans[i] == ans.size()-(i)){
+            cout << ans.size()-i << " ";
+        }
+    }
+}
