2977ca4c134c803cc593fc1a71cb08472c636c6d
thanieLee <thancraft123gaming@gmail.com>
2023-11-28 20:18:20 +0800

    APCSCI Week13-14 4.cpp

--- APCSCI30/Week13-14/4.cpp
+++ APCSCI30/Week13-14/4.cpp
@@ -1,74 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-struct node {
-    char letter;
-    int endCnt = 0;
-    vector<node> children = {};
-    node *parent;
-
-    node(char c){
-        letter = c;
-        parent = new node();
-    }
-
-    node(){
-        letter = ' ';
-        
-    }
+using namespace std;
+struct TrieNode
+{
+	map<char,TrieNode*> children;
+	int prefixes;
+	bool endofword;
+	TrieNode()
+	{
+		prefixes=0;
+		endofword=false;
+	}
 };
-
-void addNode(node &par, char n, bool isEnd){
-    node child = node(n);
-    par.children.push_back(child);
-    child.parent = &par;
-    if (isEnd){
-        child.endCnt++;
-        node& cur = par;
-        while (cur.letter != ' '){
-            //cout << "test" << " " << cur.letter << " " << n << endl;
-            cur.endCnt++;
-            //cout << "huh" << cur.parent->letter << "e"<< endl;
-            cur = *(cur.parent);
-        }
-        //cout << "damn" << endl;
-    }
+void insert(TrieNode *root,string word)
+{
+	TrieNode *current=root;
+	for(int i=0;i<word.size();i++)
+	{
+		char ch=word[i];
+		TrieNode *node=current->children[ch];
+		if(!node)
+		{
+			node=new TrieNode();
+			current->children[word[i]]=node;
+		}
+		current=node;
+		current->prefixes++;
+	}
+	current->endofword=true;
 }
-
-node findChild(node &par, char n){
-    node temp = node();
-    node &ans = temp;
-    for (node &child : par.children){
-        cout << child.letter << " " << n << endl;
-        if (child.letter == n){
-            ans = (child);
-        }
-    }
-    //cout << ans.letter << " haha" <<endl;
-    return ans;
+bool search(TrieNode *root,string word)
+{
+	TrieNode *current=root;
+	for(int i=0;i<word.size();i++)
+	{
+		char ch=word[i];
+		TrieNode *node=current->children[ch];
+		if(!node)
+			return false;
+		current=node;
+	}
+	return current->endofword;
 }
-
+int count_prefix(TrieNode *root,string word)
+{
+	TrieNode *current=root;
+	for(int i=0;i<word.size();i++)
+	{
+		char ch=word[i];
+		TrieNode *node=current->children[ch];
+		if(!node)
+		{
+			return 0;
+		}
+		current=node;
+	}
+	return current->prefixes;
+}
 
 int main(){
     int n; cin >> n;
-    node start(' ');
-
+    TrieNode *root=new TrieNode();
+    vector<int> ans;
     for (int i = 0; i < n; i++){
         string q; cin >> q;
-        node &cur = start;
         if (q == "add"){
             string s; cin >> s;
-            for (int i = 0; i < s.size(); i++){
-                bool end = false;
-                if (i == s.size()-1) end = true;
-                cout << cur.letter << endl;
-                if (findChild(cur, s[i]).letter == ' '){
-                    cout << "wut" << findChild(cur, s[i]).letter << "t" <<  endl;
-                    addNode(cur, s[i], end);
-                }
-
-                cur = findChild(cur, s[i]);
-            }
+            insert(root, s);
+        } else {
+            string s; cin >> s;
+            ans.push_back(count_prefix(root, s));
         }
     }
+
+
 }
