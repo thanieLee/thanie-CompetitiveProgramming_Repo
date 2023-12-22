af76e6f383b28377a2318acf3ce4d28675ff6d8a
thanieLee <thancraft123gaming@gmail.com>
2023-12-22 19:36:11 +0800

    Atcoder ABC185 e

---
+++ Atcoder/ABC/ABC185/e.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    int n, m; cin >> n >> m;
+    vector<int> a, b;
+    for (int i = 0; i < n; i++){
+        int x; cin >> x;
+        a.push_back(x);
+    }
+
+    for (int i = 0; i < m; i++){
+        int x; cin >> x;
+        b.push_back(x);
+    }
+}
