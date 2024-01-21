401a114d1d150c1a1de5cde6df91408afa76c338
thanieLee <thancraft123gaming@gmail.com>
2024-01-21 19:13:39 +0800

    ARC091

--- Atcoder/ARC/ARC091/e.cpp
+++ Atcoder/ARC/ARC091/e.cpp
@@ -2,6 +2,90 @@
 using namespace std;
 typedef long long int ll;
 ll INF = 10000000000;
+
+
+
+int main(){
+    int n, a, b; cin >> n >> a >> b;
+    vector<int> out(n, -1);
+    int cnt = n;
+    int blocked = 0;
+    vector<bool> done(n, false);
+
+    if (a == n and b == 1) {
+        for (int i = 1; i <= n; i++){
+            cout << i << " ";
+        }
+        return 0;
+    }
+
+    if (a == 1 and b == n) {
+        for (int i = n; i > 0; i--){
+            cout << i << " ";
+        }
+        return 0;
+    }
+
+    if (a > n or b > n) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    for (int i = 1; i < a; i++) {
+        cnt--;
+        blocked++;
+        out[n-(a-i)] = i;
+        done[i-1] = true;
+    }
+
+    int highEnd = cnt;
+    int lowEnd = cnt-a;
+    if (b > highEnd or b < lowEnd) {
+        cout << -1 << endl;
+    } else {
+        for (int i = n; i >= n-(b-1); i--){
+            out[(n) - (blocked) - (b-1 + (n - i))] = i;
+            done[i-1] = true;
+        }
+
+        int index = 0;
+        for (int i  = 1; i <= n; i++){
+            if (!done[i-1]) {
+                out[index] = i;
+                done[i-1] = true;
+                index++;
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if (out[i] == -1) {
+                cout << out[n+1] << endl;
+            }
+            cout << out[i] << " ";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+int getInvCount(vector<int> arr, int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
+ 
+    return inv_count;
+}
+
 int main(){
     int n, a, b; cin >> n >> a >> b;
     vector<int> arr;
@@ -53,3 +137,4 @@ int main(){
     } while(next_permutation(arr.begin(), arr.end()));
 
 }
+*/
