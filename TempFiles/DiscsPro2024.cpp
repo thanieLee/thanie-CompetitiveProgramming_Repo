9526eebabdc0a02cd56dc34b7fdd5e6518a15d97
thanielLee <thaniel.lee@student.ateneo.edu>
2024-04-11 19:51:14 +0800

    Discs Pro 2024

---
+++ TempFiles/DiscsPro2024.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+struct Perm{
+    int n;
+    vector<int> a;
+    Perm() {
+
+    }
+    Perm(int n) : n(n) {
+        a.resize(n);
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+        }
+    }
+
+    Perm operator* (Perm B) {
+        Perm product = Perm(n);
+        for (int i = 0; i < n; i++) {
+            product.a[i] = a[B.a[i]];
+        }
+        return product;
+    }
+};
+
+struct segtree {
+    vector<Perm> t;
+    int n, m;
+    
+    segtree(vector<Perm> &a, int n, int m1) : n(n) {
+        t.resize(4*n);
+        m = m1;
+        build(a, 1, 0, n-1);
+    }
+    void build(vector<Perm> &a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v*2, tl, tm);
+            build(a, v*2+1, tm+1, tr);
+            
+            pull(v);
+        }
+    }
+    void pull(int v) {
+        t[v] = t[v*2]*t[v*2+1];
+    }
+    void update(int v, int tl, int tr, int pos, Perm &per) {
+        if (tl == tr) {
+            t[v] = per;
+        } else {
+            int tm = (tl + tr) / 2;
+            if (pos <= tm)
+                update(v*2, tl, tm, pos, per);
+            else
+                update(v*2+1, tm+1, tr, pos, per);
+            
+            pull(v);
+        }
+    }
+
+    Perm query(int v, int tl, int tr, int l, int r) {
+        if (l > r)
+            return Perm(m);
+        if (l <= tl and tr <= r)
+            return t[v];
+        int tm = (tl + tr) / 2;
+        
+        Perm ans = query(2*v, tl, tm, l, min(r, tm))*query(2*v+1, tm+1, tr, max(l, tm+1), r);
+        return ans;
+    }
+};
+
+int main(){
+    int n, m; cin >> n >> m;
+    vector<Perm> permutations;
+    for (int i = 0; i < n; i++) {
+        Perm cur(m);
+        int l, r; cin >> l >> r; l--; r--;
+        cur.a[l] = r;
+        cur.a[r] = l;
+        permutations.push_back(cur);
+    }
+    segtree t(permutations, n, m);
+    int q; cin >> q;
+    for (int i = 0; i < q; i++) {
+        string query; cin >> query;
+        if (query == "PERM") {
+            int l, r; cin >> l >> r; l--; r--;
+            Perm ans = t.query(1, 0, n-1, l, r);
+            for (int i = 0; i < m; i++){
+                cout << ans.a[i] << " ";
+            }
+            cout << endl;
+        } else {
+            int pos, l, r; cin >> pos >> l >> r; pos--; l--; r--;
+            Perm next(m);
+            next.a[l] = r;
+            next.a[r] = l;
+            t.update(1, 0, n-1, pos, next);
+        }
+    }
+}
