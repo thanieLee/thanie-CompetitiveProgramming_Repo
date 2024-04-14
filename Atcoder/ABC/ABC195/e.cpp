e14a3c4aefa6488d5423f2c832ba3c365f4b2b31
thanieLee <thancraft123gaming@gmail.com>
2024-04-14 20:52:03 +0800

    ABC 195

--- Atcoder/ABC/ABC195/e.cpp
+++ Atcoder/ABC/ABC195/e.cpp
@@ -4,68 +4,53 @@ typedef long long int ll;
 string s, x; 
 ll n; 
 vector<vector<ll>> dp;
-vector<ll> pow10Mod7;
 
 ll solve(int i, ll md) {
     if (dp[i][md] != -1) {
         return dp[i][md];
-    }
-    if (i == n-1) {
-        ll c1 = (md+((s[i]-'0')*pow10Mod7[n-i-1])%7)%7;
-        ll c2 = md;
-        if (x[i] == 'T') {
-            if (c1 == 0 or c2 == 0) {
-                return dp[i][md] = 1;
-            }
-            return dp[i][md] = 0;
-
-        } else {
-            if (c1 == 0 and c2 == 0) {
-                return dp[i][md] = 0;
-            }
-            return dp[i][md] = 1;
+    } else if (i == n-1) {
+        ll m1 = (md*10)%7;
+        ll m2  = ((md*10)%7+(s[i]-'0'))%7;
+        if ((m1 == 0 or m2 == 0) and x[i] == 'T') {
+            return 1;
+        } else if ((m1 != 0 or m2 != 0) and x[i] == 'A') {
+            return 1;
         }
+        return 0;
     } else {
+        ll c1 = solve(i+1, ((md*10)%7+(s[i]-'0'))%7);
+        ll c2 = solve(i+1, (md*10)%7);
         char cur = x[i], next = x[i+1];
-        if (cur != next) {
-            ll c1 = solve(i+1, (md+((s[i]-'0')*pow10Mod7[n-i-1])%7)%7);
-            ll c2 = solve(i+1, md);
-            if (c1 == 0 or c2 == 0) {
-                return dp[i][md] = 1;
-            }
-            return dp[i][md] = 0;
-        } else {
-            ll c1 = solve(i+1,(md+((s[i]-'0')*pow10Mod7[n-i-1])%7)%7);
-            ll c2 = solve(i+1, md);
+        if (cur == next) {
             if (c1 == 1 or c2 == 1) {
-                return dp[i][md] = 1;
+                return dp[i][md] = true;
             }
-            return dp[i][md] = 0;
+        } else {
+            if (c1 == 0 or c2 == 0) {
+                return dp[i][md] = true;
+            }
         }
+        return dp[i][md] = false;
     }
+
 }
 
 int main(){
     cin >> n;
     cin >> s >> x;
-    ll cur = 1;
-    for (int i = 0; i <= 20000; i++) {
-        cur %= 7;
-        pow10Mod7.push_back(cur);
-        cur *= 10;
-    }
     dp.resize(n, vector<ll>(7, -1));
-    bool win = solve(0, 0);
-
-    if (win == 1) {
-        if (x[0] == 'T')
-            cout << "Takahashi" << endl;
-        else
+    ll win = solve(0, 0);
+    if (win) {
+        if (x[0] == 'A') {
             cout << "Aoki" << endl;
+        } else {
+            cout << "Takahashi" << endl;
+        } 
     } else {
-        if (x[0] == 'T')
+        if (x[0] == 'T') {
             cout << "Aoki" << endl;
-        else
+        } else {
             cout << "Takahashi" << endl;
+        } 
     }
 }
