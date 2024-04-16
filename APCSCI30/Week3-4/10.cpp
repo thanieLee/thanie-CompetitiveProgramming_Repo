608463b1de09c4834bf33fc18437c5b7af23e805
thanieLee <thancraft123gaming@gmail.com>
2024-04-16 14:45:02 +0800

    APCSCI30

--- APCSCI30/Week3-4/10.cpp
+++ APCSCI30/Week3-4/10.cpp
@@ -2,58 +2,75 @@
 using namespace std;
 typedef long long int ll;
 ll n, m, k, x;
+vector<vector<vector<ll>>> dp(100010, vector<vector<ll>>(3, vector<ll>(20)));
+vector<vector<ll>> node(3, vector<ll>(12));
+ll MOD = 1000000007ll;
 
-int main(){
-    cin >> n >> m;
-    vector<vector<ll>> adj(n, vector<ll>());
-    for (int i = 0; i < n-1; i++){
-        ll u, v; cin >> u >> v; u--; v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    cin >> k >> x;
-    deque<ll> order;
-    for (int i = 0; i < n; i++) {
-        if (adj[i].size() == 1) {
-            order.push_back(i);
-        }
-    }
-    deque<ll> q = order;
-    vector<bool> vis(n, false);
-    while(!q.empty()) {
-        ll cur = q.front();
-        q.pop_front();
-        vis[cur] = true;
+ll mod(ll x) {
+    return ((x%MOD)+MOD)%MOD;
+}
 
-        for (auto v : adj[cur]) {
-            if (!vis[v]) {
-                q.push_back(v);
-                order.push_back(v);
-                vis[v] = true;
+void dfs(ll u, ll p, vector<vector<ll>> &adj) {
+    if (adj[u].size() == 1 and p != 0) {
+        dp[u][0][0] = k-1;
+        dp[u][1][1] = 1;
+        dp[u][2][0] = m-k;
+    } else {
+        dp[u][0][0] = k-1;
+        dp[u][1][1] = 1;
+        dp[u][2][0] = m-k;
+        for (ll v: adj[u]) {
+            if (v != p) {
+                dfs(v, u, adj);
             }
         }
-    }
 
-    ll root = order.back();
-    vis.clear();
-    vis.resize(n, false);
-    vector dp(n, vector(x+1, vector<ll>(3, 0)));
 
-    for (int i = 0; i < n; i++) {
-    while (!order.empty()) {
-        ll cur = order.front();
-        order.pop_front();
-        vis[cur] = true;
-
-        if (adj[cur].size() == 1) {
-
-        } else {
-            for (auto v : adj[cur]) {
-                if (vis[v]) {
-                    
+        for (ll v : adj[u]) {
+            if (v == p) continue;
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j <= x; j++) {
+                    node[i][j] = 0;
+                }
+            }
+            for (int i = 0; i <= x; i++) {
+                for (int j = 0; j <= x-i; j++) {
+                    node[1][i+j] += mod(mod(dp[v][0][j])*dp[u][1][i]);
+                    node[1][i+j] = mod(node[1][i+j]);
+                    node[0][i+j] += mod(mod(dp[v][0][j] + dp[v][1][j] + dp[v][2][j])*dp[u][0][i]);
+                    node[0][i+j] = mod(node[0][i+j]);
+                    node[2][i+j] += mod(mod(dp[v][0][j] + dp[v][2][j])*dp[u][2][i]);
+                    node[2][i+j] = mod(node[2][i+j]);
+                } 
+            }
+            
+            for (int i = 0; i  < 3; i++) {
+                for (int j = 0; j <= x; j++){
+                    dp[u][i][j] = node[i][j];
                 }
             }
         }
     }
+}
 
+
+int main() {
+    cin >> n >> m;
+    vector<vector<ll>> adj(n+1, vector<ll>());
+    for (int i = 0; i < n-1; i++) {
+        ll ui, vi; cin >> ui >> vi;
+        adj[ui].push_back(vi);
+        adj[vi].push_back(ui);
+    }
+    cin >> k >> x;
+    dfs(1, 0, adj);
+    ll ans = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j <= x; j++) {
+            ans += dp[1][i][j];
+            //cout << dp[1][i][j] << " " << i << " " << j <<  endl;
+            ans = mod(ans);
+        }
+    }
+    cout << ans << endl;
 }
