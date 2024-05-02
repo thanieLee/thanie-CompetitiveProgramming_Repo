cb95ba875d4cb1d69a2cdde41bd037860226dc14
thanieLee <thancraft123gaming@gmail.com>
2024-05-02 12:29:19 +0800

    APCSCI30 Week13-14

--- APCSCI30/Week13-14/8.cpp
+++ APCSCI30/Week13-14/8.cpp
@@ -1,71 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, N;
-vector<int> equiv, sa, pos;
-vector<pair<int, int>> equiv_pair;
-vector<int> lcp;
+class Node
+{
+public:
+    char ch;
+    unordered_map<char, Node*> children;
+    vector<int> indexes; //store the indexes of the substring from where it starts
+    Node(char c):ch(c){}
+};
 
-void suffix() {
-    for (int i = 0; i < N; i++)
-        sa[i] = i, pos[i] = S[i];
+int maxLen = 0;
+string maxStr = "";
 
-    for (gap = 1;; gap <<= 1) {
-        sort(sa, sa+N, comp);
-        for (int i = 0; i < N-1; i++)
-            tmp[i+1] = tmp[i] + comp(sa[i], sa[i+1]);
-        for (int i = 0; i < N; i++)
-            pos[sa[i]] = tmp[i];
-        if (tmp[N - 1] == N - 1)
-            break;
+void insertInSuffixTree(Node* root, string str, int index, string originalSuffix, int level=0)
+{
+    root->indexes.push_back(index);
+
+    // it is repeated and length is greater than maxLen
+    // then store the substring
+    if(root->indexes.size() > 1 && maxLen < level)
+    {
+        maxLen = level;
+        maxStr = originalSuffix.substr(0, level);
     }
+
+    if(str.empty()) return;
+
+    Node* child;
+    if(root->children.count(str[0]) == 0) {
+        child = new Node(str[0]);
+        root->children[str[0]] = child;
+    } else {
+        child = root->children[str[0]];
+    }
+
+    insertInSuffixTree(child, str.substr(1), index, originalSuffix, level+1);
 }
 
+int main()
+{
+    string str; cin >> str;
+    Node* root = new  Node('@');
 
-void buildLCP(string& s){
-    vector<int> inv(n+1);
-
-    for (int i = 0; i < suffix.size(); i++){
-        inv[suffix[i]] = i;
+    //insert all substring in suffix tree
+    for(int i=0; i<str.size(); i++){
+        string s = str.substr(i);
+        insertInSuffixTree(root, s, i, s);
     }
-    
-    for (int i = 0, k = 0; i < n; i++){
-        if (inv[i] != n-1) {
-            for (int j = suffix[inv[i] + 1]; s[i+k] == s[j+k]; k++);
-                lcp[inv[i]] = k;
-                if (k > 0) k--;
-        } 
-    }
-}
-
-
-
-int main(){
-    string s; cin >> s;
-    n = s.size();
-    equiv.resize(n+1);
-    suffix.resize(n+1);
-    equiv_pair.resize(n+1);
-    lcp.resize(n, 0);
-    make_suffix_array(s);
-    buildLCP(s);
-    
-    pair<int, int> cur = {0, 0};
-
-    for (int i = 0; i < n; i++){
-        if (lcp[i] > cur.first){
-            cur = {lcp[i], i};
-        }
-    }
-
-    if (cur.first == 0) {
+    if (maxLen > 0)
+        cout << maxStr << endl;
+    else
         cout << -1 << endl;
-        return 0;
-    }
 
-    for (int i = 0; i < cur.first; i++){
-        cout << s[suffix[cur.second] + i];
-    }
-    cout << endl;
-
+    return 1;
 }
+
