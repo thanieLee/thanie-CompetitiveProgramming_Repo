e767795c7b6df189c8b59ad248a10d74cc5179b0
thanieLee <thancraft123gaming@gmail.com>
2024-06-07 00:51:25 +0800

    DP: Classic Problems with a Twist

--- ProgVar.Fun/DP/Classic Problems with a Twist/3.cpp
+++ ProgVar.Fun/DP/Classic Problems with a Twist/3.cpp
@@ -44,19 +44,46 @@ int main(){
         }
     }
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++) {
-            cout << dp1[i][j] << " ";
+    vector<vector<ll>> dp3(n, vector<ll>(m, 0));
+    for (int i = n-1; i >= 0; i--) {
+        for (int j = m-1; j >= 0; j--) {
+            if (i == n-1 and j == m-1) {
+                dp3[i][j] = graph[i][j];
+            } else if (i == n-1) {
+                dp3[i][j] = dp3[i][j+1]+graph[i][j];
+            } else if (j == m-1) {
+                dp3[i][j] = dp3[i+1][j]+graph[i][j];
+            } else {
+                dp3[i][j] = max(dp3[i+1][j], dp3[i][j+1]) + graph[i][j];
+            }
         }
-        cout << endl;
     }
-    cout << endl;
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++) {
-            cout << dp2[i][j] << " ";
+    vector<vector<ll>> dp4(n, vector<ll>(m, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = m-1; j >= 0; j--) {
+            if (i == 0 and j == m-1) {
+                dp4[i][j] = graph[i][j];
+            } else if (i == 0) {
+                dp4[i][j] = dp4[i][j+1]+graph[i][j];
+            } else if (j == m-1) {
+                dp4[i][j] = dp4[i-1][j]+graph[i][j];
+            } else {
+                dp4[i][j] = max(dp4[i-1][j], dp4[i][j+1]) + graph[i][j];
+            }
         }
-        cout << endl;
     }
 
+    
+    ll ans = 0;
+    for (int i = 1; i < n-1; i++) {
+        for (int j = 1; j < m-1; j++) {
+            ll case1 = dp1[i-1][j]+dp2[i][j-1]+dp3[i+1][j]+dp4[i][j+1];
+            ll case2 = dp1[i][j-1]+dp2[i+1][j]+dp3[i][j+1]+dp4[i-1][j];
+
+            ans = max(ans, max(case1, case2));
+        }
+    }
+
+    cout << ans << endl;
 }
