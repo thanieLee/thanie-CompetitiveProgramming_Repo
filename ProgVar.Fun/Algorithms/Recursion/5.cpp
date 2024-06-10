ee68f368d17010b2be8103ced98e291d918ad216
thanieLee <thancraft123gaming@gmail.com>
2024-06-10 19:08:23 +0800

    Recursion

--- ProgVar.Fun/Algorithms/Recursion/5.cpp
+++ ProgVar.Fun/Algorithms/Recursion/5.cpp
@@ -1,36 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+typedef pair<int, int> pii;
 
-vector<vector<int>> solve(vector<vector<int>> order, int dep, int end) {
-    if (dep == end) return order;
-    vector<vector<int>> next(dep*2, vector<int>(dep*2));
-
-    vector<vector<int>> first(order.size(), vector<int>(order.size()));
-    for (int i = 0; i < dep; i++) {
-        for (int j = 0; j < dep; j++) {
-            first[i][j] = order[j][i];
-        }
-    }
-
-    vector<vector<int>> second = order;
-    vector<vector<int>> third(order.size(), vector<int>(order.size()));
-
-    for (int i = 0; i < dep; i++) {
-        for (int j = 0; j < dep; j++) {
-              third[i][j] = order[]  
-        }
+pii solve(int dep, int step) {
+    if (dep == 1) return {0, 0};
+    int half = dep/2;
+    if ((step / (half*half)) == 1) {
+        return solve(half, step-half*half);
+    } else if ((step/(half*half)) == 2) {
+        pii next = solve(half, step-half*half*2);
+        return {next.first, next.second+half};
+    } else if ((step/(half*half)) == 0) {
+        pii next = solve(half, step);
+        return {half-next.second+half-1, half-next.first-1};
+    } else {
+        pii next = solve(half, step-half*half*3);
+        return {next.second+half, next.first+half};
     }
 }
 
 int main(){
     int n, m; cin >> n >> m;
-
-    if (n == 1) {
-        cout << 1 << " " << 1 << endl;
-        return;
-    }
-
-    vector<vector<int>> order = {{1, 4}, {2, 3}};
-
+    pii ans = solve(n, m-1);
+    cout << ans.second+1 << " " << n-ans.first << endl;
 }
