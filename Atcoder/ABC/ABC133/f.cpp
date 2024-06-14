fbe82ea47b074cf770f67cbeeddbc465df606078
thanielLee <thaniel.lee@student.ateneo.edu>
2024-06-14 18:56:27 +0800

    CSES and ABC133

--- Atcoder/ABC/ABC133/f.cpp
+++ Atcoder/ABC/ABC133/f.cpp
@@ -1,117 +1,151 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+typedef pair<ll, ll> pll;
 
 
-struct color_graph {
-	int n, l;
-	vector<vector<int>> adj, par;
-	vector<int> dep;
 
-	color_graph(int n) : n(n) {
-		l = ceil(log2(n));
-		dep.resize(n);
-		adj.resize(n, vector<int>());
-		par.resize(n, vector<int>(l+1));
+struct segtree {
+    vector<pll> t;
+    int n;
+
+    segtree(vector<pll> &a, int n) : n(n) {
+        t.resize(4*n);
+        build(a, 1, 0, n-1);
+    }
+    void build(vector<pll> &a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v*2, tl, tm);
+            build(a, v*2+1, tm+1, tr);
+            
+            pull(v);
+        }
+    }
+    void pull(int v) {
+        t[v] = min(t[v*2], t[v*2+1]);
+    }
+    void update(int v, int tl, int tr, int pos, pll val) {
+        if (tl == tr) {
+            t[v] = val;
+        } else {
+            int tm = (tl + tr) / 2;
+            if (pos <= tm)
+                update(v*2, tl, tm, pos, val);
+            else
+                update(v*2+1, tm+1, tr, pos, val);
+            
+            pull(v);
+        }
+    }
+    pll query(int v, int tl, int tr, int l, int r) {
+        if (l > r)
+            return pll(1000000, 1000000);
+        if (l == tl and r == tr)
+            return t[v];
+        int tm = (tl + tr) / 2;
+        return min(query(v*2, tl, tm, l, min(r, tm)), query(v*2+1, tm+1, tr, max(l, tm+1), r));
+    }
+};  
+
+struct point {
+	ll next, c, d;
+	point(ll v, ll c, ll d) : next(v), c(c), d(d) {
+
 	}
+};
 
-	void dfs(int u, int p, int d) {
-		dep[u] = d;
-		par[u][0] = p;
-		for (int i = 1; i <= l; i++)
-			par[u][i] = par[par[u][i-1]][i-1];
+vector<pll> arr;
+vector<pll> euler;
+vector<int> colors;
+vector<vector<pll>> color_euler, color_euler_count;
+vector<vector<point>> adj;
+vector<int> depth;
+vector<ll> dist;
+vector<int> start, en;
+int timer = 0;
 
-		for (int v : adj[u]) {
-			if (v != p)
-				dfs(v, u, d+1);
-		}
-	}
-	int ascend(int u, int k) {
-		for (int i = 0; i <= l; i++)
-			if (k & (1 << i))
-				u = par[u][i];
-		return u;
-	}
-	bool is_anc(int u, int v) {
-		if (dep[u] < dep[v])
-			swap(u, v);
-		return ascend(u, dep[u]-dep[v]) == v;
-	}
-	int lca(int u, int v) {
-		if (dep[u] > dep[v])
-			u = ascend(u, dep[u]-dep[v]);
-		if (dep[v] > dep[u])
-			v = ascend(v, dep[v]-dep[u]);
-		if (u == v)
-			return u;
 
-		for (int i = l; i >= 0; i--) {
-			if (par[u][i] != par[v][i]) {
-				u = par[u][i];
-				v = par[v][i];
-			}
-		}
-		return par[u][0];
-	}
-}; // Populate adjacency array then call dfs(root, root, 0)
+void dfs1(int u, int p, ll dep, ll distan) {
+    arr.push_back({dep, u});
+    start[u] = arr.size()-1;
+    depth[u] = dep;
+	dist[u] = distan;
 
-struct main_graph {
-	int n, l;
-	vector<vector<int>> adj, par;
-	vector<int> dep;
-    vector<color_graph> color_graphs;
-    vector<bool> color_visited;
-    vector<int> colors;
+    for (point v : adj[u]) {
+        if (v.next != p) {
+			euler.push_back({euler.size(), v.d+euler[euler.size()-1].second});
+			color_euler[v.c].push_back({euler.size()-1, v.d+color_euler[v.c][color_euler[v.c].size()-1].second});
+			color_euler_count[v.c].push_back({euler.size()-1, 1+color_euler_count[v.c][color_euler_count[v.c].size()-1].second});
+			dfs1(v.next, u, dep+1, distan+v.d);
+			euler.push_back({euler.size(), -v.d+euler[euler.size()-1].second});
+			color_euler[v.c].push_back({euler.size()-1, -v.d+color_euler[v.c][color_euler[v.c].size()-1].second});
+			color_euler_count[v.c].push_back({euler.size()-1, -1+color_euler_count[v.c][color_euler_count[v.c].size()-1].second});
+            arr.push_back({dep, u});
+        }
+    }
+    en[u] = arr.size()-1;
+}
 
-	main_graph(int n) : n(n) {
-		l = ceil(log2(n));
-		dep.resize(n);
-		adj.resize(n, vector<int>());
-		par.resize(n, vector<int>(l+1));
-        colors.resize(n);
-	}
+bool cmp(pll &x, pll &y) {
+	return x.first < y.first;
+}
 
-	void dfs(int u, int p, int d) {
-		dep[u] = d;
-		par[u][0] = p;
-		for (int i = 1; i <= l; i++)
-			par[u][i] = par[par[u][i-1]][i-1];
-
-		for (int v : adj[u]) {
-			if (v != p)
-				dfs(v, u, d+1);
-		}
-	}
-	int ascend(int u, int k) {
-		for (int i = 0; i <= l; i++)
-			if (k & (1 << i))
-				u = par[u][i];
-		return u;
-	}
-	bool is_anc(int u, int v) {
-		if (dep[u] < dep[v])
-			swap(u, v);
-		return ascend(u, dep[u]-dep[v]) == v;
-	}
-	int lca(int u, int v) {
-		if (dep[u] > dep[v])
-			u = ascend(u, dep[u]-dep[v]);
-		if (dep[v] > dep[u])
-			v = ascend(v, dep[v]-dep[u]);
-		if (u == v)
-			return u;
-
-		for (int i = l; i >= 0; i--) {
-			if (par[u][i] != par[v][i]) {
-				u = par[u][i];
-				v = par[v][i];
-			}
-		}
-		return par[u][0];
-	}
-}; // Populate adjacency array then call dfs(root, root, 0)
-
-int main() {
+int main(){
     int n, q; cin >> n >> q;
-    
+    adj.resize(n, {});
+    start.resize(n, -1);
+    en.resize(n, -1); 
+	euler.resize(1, {-1, 0});
+    depth.resize(n, -1);
+	dist.resize(n, -1);
+	color_euler.resize(n, {{-1, 0}});
+	colors.resize(n, -1);
+	color_euler_count.resize(n, {{-1, 0}});
+    for (int i = 0; i < n-1; i++) {
+        ll a, b, c, d; cin >> a >> b >> c >> d; a--; b--; c--;
+        adj[a].push_back({b, c, d});
+        adj[b].push_back({a, c, d});
+    }
+
+    dfs1(0, -1, 0, 0);
+    segtree lca(arr, arr.size());
+	for (int i = 0; i < euler.size(); i++) {
+		cout << euler[i].first << " " << euler[i].second << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < n-1; i++) {
+		vector<pll> cur = color_euler[i];
+		for (int j = 0; j < cur.size(); j++) {
+			cout << cur[j].first << " " << cur[j].second << endl;
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < q; i++) {
+		ll x, y, u, v; cin >> x >> y >> u >> v; x--; u--; v--;
+		ll fir = start[u], sec = start[v];
+		if (fir > sec) swap(fir, sec);
+
+		ll par = lca.query(1, 0, arr.size()-1, fir, sec).second;	
+		ll total = dist[u]+dist[v] - 2*dist[par];
+		pll search1(fir, -1), search2(sec, -1);
+		auto p1 = upper_bound(color_euler[x].begin(), color_euler[x].end(), search1);
+		auto p2 = upper_bound(color_euler[x].begin(), color_euler[x].end(), search2);
+		p1--; p2--;
+		ll color_start = p1->second;
+		ll color_end = p2->second;
+		ll final_dist = total - (color_end-color_start);
+		auto p3 = upper_bound(color_euler_count[x].begin(), color_euler_count[x].end(), search1);
+		auto p4 = upper_bound(color_euler_count[x].begin(), color_euler_count[x].end(), search2);
+		p3--; p4--;
+		ll cnt_start = p3->second;
+		ll cnt_end = p4->second;
+		cout << fir << " " << sec << endl;
+		cout << total << " " << final_dist << endl;
+		cout << color_start << " " << color_end << endl;
+		cout << cnt_end << " " << cnt_start << endl;
+		cout << final_dist + y*(cnt_end-cnt_start) << endl;
+	}
 }
---
+++ CSES/Graph/1688.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+
+
+struct segtree {
+    vector<pll> t;
+    int n;
+
+    segtree(vector<pll> &a, int n) : n(n) {
+        t.resize(4*n);
+        build(a, 1, 0, n-1);
+    }
+    void build(vector<pll> &a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v*2, tl, tm);
+            build(a, v*2+1, tm+1, tr);
+            
+            pull(v);
+        }
+    }
+    void pull(int v) {
+        t[v] = min(t[v*2], t[v*2+1]);
+    }
+    void update(int v, int tl, int tr, int pos, pll val) {
+        if (tl == tr) {
+            t[v] = val;
+        } else {
+            int tm = (tl + tr) / 2;
+            if (pos <= tm)
+                update(v*2, tl, tm, pos, val);
+            else
+                update(v*2+1, tm+1, tr, pos, val);
+            
+            pull(v);
+        }
+    }
+    pll query(int v, int tl, int tr, int l, int r) {
+        if (l > r)
+            return pll(1000000, 1000000);
+        if (l == tl and r == tr)
+            return t[v];
+        int tm = (tl + tr) / 2;
+        return min(query(v*2, tl, tm, l, min(r, tm)), query(v*2+1, tm+1, tr, max(l, tm+1), r));
+    }
+};  
+
+vector<pll> arr;
+vector<vector<int>> adj;
+vector<int> depth;
+vector<int> start, en;
+int timer = 0;
+void dfs(int u, int p, ll dep) {
+    arr.push_back({dep, u});
+    start[u] = arr.size()-1;
+    depth[u] = dep;
+
+    for (int v : adj[u]) {
+        if (v != p) {
+            dfs(v, u, dep+1);
+            arr.push_back({dep, u});
+        }
+    }
+    en[u] = arr.size()-1;
+}
+
+
+int main(){
+    int n, q; cin >> n >> q;
+    adj.resize(n, vector<int>());
+    start.resize(n, -1);
+    en.resize(n, -1);
+    depth.resize(n, -1);
+
+    for (int i = 1; i < n; i++) {
+        int par; cin >> par; par--;
+        adj[par].push_back(i);
+        adj[i].push_back(par);
+    }
+
+    dfs(0, -1, 0);
+    segtree t(arr, arr.size());
+    // for (int i = 0; i < arr.size(); i++) {
+    //     cout << arr[i].second+1 << " ";
+    // }
+    // cout << endl;
+    // for (int i = 0; i < arr.size(); i++) {
+    //     cout << arr[i].first+1 << " ";
+    // }
+    // cout << endl;
+    // for (int i = 0; i < n; i++) {
+    //     cout << start[i] << " ";
+    // }
+    // cout << endl;
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << en[i] << " ";
+    // }
+    // cout << endl;
+
+    for (int i = 0; i < q; i++) {
+        int a, b; cin >> a >> b; a--; b--;
+        ll fir = start[a], sec = en[b];
+        if (fir > sec) swap(fir, sec);
+        cout << t.query(1, 0, arr.size()-1, fir, sec).second+1 << endl;
+    }
+
+}
